7427112ad440fda04ed6eee6d1940461b30cbe75
M Daniel Tays <mdanieltays@gmail.com>
2017-08-31 20:07:41 -0700

    Add React-MDL and all of it's components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import * from 'react-mdl';
+import { Button, Card, CardText } from 'react-mdl';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
